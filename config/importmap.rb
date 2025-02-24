228ff3bf5c1ad080db7165d07475d2cc49ccfd3f
gameroster-us <gameroster-us@gmail.com>
2025-02-24 12:10:59 +0530

    Added new home page and fix jquery issue

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
+pin "jquery", to: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "popper", to: 'popper.js', preload: true
