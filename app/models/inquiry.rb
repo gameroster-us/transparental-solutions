38b0d70de80da383bbe16109ab93ea773b3253ff
gameroster-us <gameroster-us@gmail.com>
2025-03-10 10:31:38 +0530

    Added validation

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,2 +1,8 @@
 class Inquiry < ApplicationRecord
+  validates :name, :message, :website, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+  validates :phone_number, presence: true, format: { 
+    with: /\A\d{10}\z/, 
+    message: "must be exactly 10 digits and contain only numbers"
+  }
 end
